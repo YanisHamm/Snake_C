d0e760288114049a6de8011518a6f0d06e17bade
Yanis <yyanis20@gmail.com>
2022-02-26 23:17:52 +0100

    Version  0.9 du projet

---
+++ Snake 0.9/case_plateau.h
@@ -0,0 +1,19 @@
+#ifndef CASE_PLATEAU_H_INCLUDED
+#define CASE_PLATEAU_H_INCLUDED
+#include <stdbool.h>
+
+typedef struct case_plateau
+{
+    bool obstacle;
+    bool fruit;
+    bool snake;
+    bool vide;
+    char lettre_fruit;
+    int x;
+    int y;
+    int idsnake;
+} case_plateau;
+
+
+
+#endif // CASE_PLATEAU_H_INCLUDED
